e8daaf24f190e8c608bdd608d66fb4f20ac3433c
VietMX <vietmx@fincorp.com>
2021-10-10 22:54:55 +0700

    init source code

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+import Counter from "./counter"
+
+export default function Home() {
+  return <div style={{ color: 'tomato' }}>
+    <h1>Hello world!</h1>
+    <p>Welcome to my first Gatsby site</p>
+    <br></br>
+    <br></br>
+    <Counter></Counter>
+    <br></br>
+    <br></br>
+    <Link to="/page-2/">Page 2</Link>
+    <br></br>
+    <Link to="/counter/">Counter</Link>
+  </div>
+}
